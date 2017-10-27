2471ab63bef495061da0dd0966954ec0e922b2bf
Jessie <jess.bell@me.com>
2017-10-27 11:59:29 -0700

    Add wire-frame outline for body of index.html, and start to add media queries for grid component

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
-}
+
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
